32f6645893f374770d05eefa0008c9a9578f9560
Guillaume Dujardin <gdu@eonix.be>
2020-01-27 08:44:23 +0100

    Added UT's for ReadPaginatedHandler

---
+++ test/Test.CleanArch.Utilities.GenericCrud/_TestArtifacts/MockReadPaginatedRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using CleanArch.Utilities.GenericCrud.Services.ReadPaginated;
+
+namespace Test.CleanArch.Utilities.GenericCrud._TestArtifacts
+{
+    public class MockReadPaginatedRequest : ReadPaginatedRequest<MockEntity, Guid>
+    {
+        public override IQueryable<MockEntity> AddWhereConditions(IQueryable<MockEntity> queryable) 
+            => queryable;
+    }
+}
